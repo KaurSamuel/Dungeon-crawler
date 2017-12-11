f3f45fa36fe202a71f49db17e1b01d1605a5f74a
hindrekhan <hindrek.hannus@tptlive.ee>
2017-12-11 11:43:20 +0200

    asdf

--- ConsoleApp1/ConsoleApp1/Dungeon/Dungeon.cs
+++ ConsoleApp1/ConsoleApp1/Dungeon/Dungeon.cs
@@ -12,16 +12,20 @@ namespace ConsoleApp1
         List<Room> MapArray = new List<Room>();
         Vector2 CurPos;
 
-        public enum Sides {None = 0, Up = 1, Down = 2, Left = 4, Right = 8};
+        int SidesNone = 0;
+        int SidesUp = 1;
+        int SidesDown = 2;
+        int SidesLeft = 4;
+        int SidesRight = 8;
 
         public struct Room
         {
             public string roomData;
             public Vector2 pos;
             public Vector2 size;
-            public Sides sides;
+            public int sides;
             
-            public Room(Vector2 Pos, Vector2 Size, string RoomData, Sides Sides)
+            public Room(Vector2 Pos, Vector2 Size, string RoomData, int Sides)
             {
                 roomData = RoomData;
                 pos = Pos;
@@ -33,6 +37,11 @@ namespace ConsoleApp1
         public Dungeon()
         {
             GenerateMap();
+        }
+
+        bool CheckSide(byte curSide, int bit)
+        {
+            return (curSide & (1 << bit)) != 0;
         }
 
         static string ReplaceAtIndex(int i, char value, string word)
@@ -55,44 +64,43 @@ namespace ConsoleApp1
             newPos.X = rnd.Next((int)posTopLeft.X, (int)posBotRight.X);
             newPos.Y = rnd.Next((int)posTopLeft.Y, (int)posBotRight.Y);
 
-            room = GenerateRoom(newPos, (Sides)rnd.Next(0, 16));
+            room = GenerateRoom(newPos, (int)rnd.Next(0, 16));
 
             CurPos = room.pos;
 
-            room = AddDoor(room, Sides.Up);
+            room = AddDoor(room, SidesUp);
 
             return room;
         }
-
-        public Room AddDoor(Room room, Sides sides)
+        
+        public Room AddDoor(Room room, int sides)
         {
-            if (sides == Sides.Up)
-                room.roomData = ReplaceAtIndex((int)room.size.X / 2, ' ', room.roomData);
+            room.roomData = ReplaceAtIndex((int)room.size.X / 2, ' ', room.roomData);
 
             room.sides = sides;
 
             return room;
         }
 
-        public Room GenerateRoom(Vector2 pos = default(Vector2), Sides sides = 0)
+        public Room GenerateRoom(Vector2 pos = default(Vector2), int sides = 0)
         {
             string roomData = "";
-            int roomWidth = 8;
-            int roomHeight = 16;
+            int roomWidth = 16;
+            int roomHeight = 8;
 
             // Bug: Currently roomWidth and roomHeight are reversed
             
-            for (int cur_width = 0; cur_width < roomWidth; cur_width++)
+            for (int cur_height = 0; cur_height < roomHeight; cur_height++)
             {
-                for (int cur_height = 0; cur_height < roomHeight; cur_height++)
+                for (int cur_width = 0; cur_width < roomWidth; cur_width++)
                 {
                     char appendable = ' ';
 
                     if (cur_width == 0 || cur_width == roomWidth - 1)
-                        appendable = '-';
+                        appendable = '|';
 
                     if (cur_height == 0 || cur_height == roomHeight - 1)
-                        appendable = '|';
+                        appendable = '-';
 
                     roomData += appendable;
                 }
@@ -100,7 +108,7 @@ namespace ConsoleApp1
                 roomData += '\n';
             }
 
-            return new Room(new Vector2(25, 25), new Vector2(roomWidth, roomHeight), roomData, Sides.None);
+            return new Room(new Vector2(25, 25), new Vector2(roomWidth, roomHeight), roomData, SidesNone);
         }
 
         public bool GenerateMap()
@@ -113,18 +121,24 @@ namespace ConsoleApp1
         public bool DrawMap()
         {
             string buffer = "";
-            Sides sides;
-            Room curRoom;
+            int sides;
+            Room curRoom = new Room();
             Vector2[] sideRooms;
+
+            Console.Clear();
 
             foreach (Room room in MapArray)
                 if (room.pos == CurPos)
                     curRoom = room;
+                else
+                    throw new System.ArgumentException("CurRoom not found");
 
-            sideRooms = new Vector2[];
+            //sideRooms = new Vector2[Vector2];
 
-            foreach (Room room in MapArray)
-                
+            //foreach (Room room in MapArray)
+
+            Console.WriteLine(curRoom.roomData);
+            Console.WriteLine(buffer);
 
             return true;
         }
