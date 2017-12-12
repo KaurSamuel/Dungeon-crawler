21b3d11a1e7247d8fb3f5204192f342778c8a7dc
KaurSamuel <33308949+KaurSamuel@users.noreply.github.com>
2017-12-12 08:56:47 +0200

    xd

---
+++ ConsoleApp1/ConsoleApp1/Player Values.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Player_Values
+    {
+        public string Player_level { get; set; }
+        public string Player_current_exp { get; set;}
+        public string Player_block_chance { get; set; }
+        public string Player_rooms_cleared { get; set; }
+        public string Player_monsters_killed { get; set; }
+        public static List<weapons> Weapons_Inventory = new List<weapons>();
+    }
+
+
+    class Player_Inventory
+    {
+
+
+
+
+
+
+        public static String Add_to_inventory(string Item)
+        {
+
+
+               
+
+
+
+            return ("");
+        }
+    }
+}
