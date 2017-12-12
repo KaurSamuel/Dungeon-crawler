fbbdb883f6623534ac37b52c9a6c23fa5b92f9b9
hindrekhan <hindrek.hannus@tptlive.ee>
2017-12-12 13:09:48 +0200

    partially implemented monster exporter

---
+++ ConsoleApp1/ConsoleApp1/Convertor2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Convertor2
+    {
+        public static string export_mon(string ID, string whatvalue)
+        {
+            string ValueType;
+            if (whatvalue == "mob_Name")
+            {
+                ValueType = "1";
+            }
+
+            else if (whatvalue == "mob_hp")
+            {
+                ValueType = "2";
+            }
+
+            else if (whatvalue == "mob_max_dmg")
+            {
+                ValueType = "3";
+            }
+
+            else if (whatvalue == "mob_min_dmg")
+            {
+                ValueType = "4";
+            }
+
+            else if (whatvalue == "mob_exp")
+            {
+                ValueType = "5";
+            }
+
+            string DirectoryPath = "monsters.txt";
+
+
+        }
+    }
+}
