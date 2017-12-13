c465d81d867bf4d52553a9b7380b62fef5347dce
hindrekhan <hindrek.hannus@tptlive.ee>
2017-12-13 11:28:23 +0200

    updated summary

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,10 +14,7 @@ namespace ConsoleApp1
              * Your Mission is to defeat four evil Program Developers,
              * because they have diabolical plan to take over internet and spread pornographic viruses.
              * Your our last hope :(
-             */
-            //string relv = Convertor.exporter_wep("wA001","wep_desc");
-            //Console.WriteLine(relv);
-            //Console.ReadLine();
+             */ //Miks see veel siin on ?
 
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
