a821d0b3796a8ef0fbe80eb2daf44e8f8694ad79
JaanJah <jaanmarkus.jahilo@tptlive.ee>
2017-12-13 09:47:23 +0200

    Revert "xd"
    This reverts commit 2f31a59f8b273ea37980170b343a4c4061f31699.

--- ConsoleApp1/ConsoleApp1/Chest.cs
+++ ConsoleApp1/ConsoleApp1/Chest.cs
@@ -6,15 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public class LootChest
+    public class Chest : Weapons
     {
-        public string Chest()
-        {
-            if (Dungeon.EnemiesAlive == false)
-            {
-                Tuple.Create(0.5, (Weapons.Inventory.Add) new Weapons(wA001));
 
-            }
-        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Dungeon/Dungeon.cs
+++ ConsoleApp1/ConsoleApp1/Dungeon/Dungeon.cs
@@ -11,7 +11,7 @@ using System.Collections;
 
 namespace ConsoleApp1
 {
-    public class Dungeon
+    class Dungeon
     {
         List<Room> MapArray = new List<Room>();
         Vector2 CurPos = new Vector2(0.0f, 0.0f);
@@ -33,10 +33,8 @@ namespace ConsoleApp1
         const byte AddSidesLeft = 2;
         const byte AddSidesRight = 3;
 
-        public static bool check { get; protected set; }
-
         //Returns true if there are any enemies alive in the room
-        public static bool EnemiesAlive { get; set; }
+        public bool EnemiesAlive { get; set; }
 
         //Holds data of each room
         public struct Room
