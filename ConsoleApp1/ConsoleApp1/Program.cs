80da0dc92d3d9bea704abfb4c0b5756bb47c3c59
hindrekhan <hindrek.hannus@tptlive.ee>
2017-12-13 12:23:46 +0200

    removed story line

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,12 +10,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /* Hiya Human, you have succesfully entered the Gaming Realm.
-             * Your Mission is to defeat four evil Program Developers,
-             * because they have diabolical plan to take over internet and spread pornographic viruses.
-             * Your our last hope :(
-             */ //Miks see veel siin on ?
-
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
             Engine engine = new Engine();
