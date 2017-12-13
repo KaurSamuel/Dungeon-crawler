9ac5e8819b2c1fc3fa419716a276fb8cbce323df
hindrekhan <hindrek.hannus@tptlive.ee>
2017-12-13 11:29:42 +0200

    comment out lootchest code

--- ConsoleApp1/ConsoleApp1/Chest.cs
+++ ConsoleApp1/ConsoleApp1/Chest.cs
@@ -8,13 +8,13 @@ namespace ConsoleApp1
 {
     public class LootChest
     {
-        public string Chest()
+        /*public string Chest()
         {
             if (Dungeon.EnemiesAlive == false)
             {
                 Tuple.Create(0.5, (Weapons.Inventory.Add) new Weapons(A001));
 
             }
-        }
+        }*/
     }
 }
